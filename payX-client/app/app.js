32b54098ac11295a9ca7a762f852070d89df11e1
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-06 11:47:06 +0700

    Init commit for payX client

---
+++ payX-client/app/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import HomePage from './containers/HomePage';
+
+ReactDOM.render(
+    <HomePage />,
+    document.getElementById('container')
+);

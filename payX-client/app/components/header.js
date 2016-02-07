e641ab0403becdfae8cadaddd6ddc09aa585ea3d
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-08 03:32:32 +0700

    add header

---
+++ payX-client/app/components/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <div className='navbar-brand' style={{'text-align': 'center', 'width': '100%'}}>
+              {this.props.tittle}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}

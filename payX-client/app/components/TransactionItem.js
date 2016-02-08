7d40fdfd6b2aa82c62454acf3dff95ac6cf969d0
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-08 22:35:40 +0700

    style transaction item

--- payX-client/app/components/TransactionItem.js
+++ payX-client/app/components/TransactionItem.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 export default class TransactionItem extends Component {
   render() {
+    const transaction = this.props.transaction;
+    const transactionDate = `${transaction.createdAt.getDate()}/${transaction.createdAt.getMonth()+1}/${transaction.createdAt.getFullYear()}`;
     return (
-      <tr scope='row odd' className='transaction-item'>
-        <td>{this.props.transaction.createdAt.toString()}</td>
+      <tr scope='row' className='transaction-item'>
+        <td>{transactionDate}</td>
         <td>{this.props.transaction.to}</td>
         <td>{this.props.transaction.currency.symbol + '' + this.props.transaction.amount}</td>
       </tr>

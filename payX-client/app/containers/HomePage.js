0ed52836d7236286c200cfcaa477ab33ae92821b
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-08 22:43:20 +0700

    style the home page

--- payX-client/app/containers/HomePage.js
+++ payX-client/app/containers/HomePage.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { Link } from 'react-router'
 import { connect } from 'react-redux';
 import { routeActions } from 'react-router-redux';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const HomePage = ({push, children}) => {
 
@@ -17,19 +19,23 @@ const HomePage = ({push, children}) => {
 
   return (
     <div className="HomePage">
-      HomePage2
+      <Header
+        tittle='What are we doing?'
+      />
       <input
-        className='form-control'
+        className='col-xs-12 btn btn-primary btn-lg vertical-padding-s'
         type="button"
         onClick={onClickSendMoney}
         value='Send money'
       />
       <input
-        className='form-control'
+        className='col-xs-12 btn btn-primary btn-lg vertical-padding-s'
         type="button"
         onClick={onClickViewTransactions}
         value='View transactions history'
       />
+      <Footer
+        />
     </div>
   )
 }

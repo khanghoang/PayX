bf8bcca87ba5e0fc1a8077225f20b5bcf0301a41
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-09 00:21:32 +0700

    add dummy server

--- payX-server/app/index.js
+++ payX-server/app/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
+import bodyParser from 'body-parser';
 import bluebird from 'bluebird';
 const server = express();
 
@@ -11,20 +12,29 @@ function loadFile() {
   });
 }
 
+server.use(bodyParser.urlencoded({extended: true}));
 
-server.get('/', async (req, res) => {
-  let content;
-  try {
-    content = await loadFile();
-  } catch (e) {
-    console.log(e);
-  }
-
-  res.send('hello world 3');
+server.post('/send_money', async (req, res) => {
+  res.status(200).json({
+    ...req.body,
+    ...{success: true},
+    ...{createdAt: new Date()}
+  });
 });
 
-server.listen(8080, (err) => {
+server.get('/transactions_history', async (req, res) => {
+  res.status(200).json({
+    per_page: 10,
+    total_pages: 5,
+    current_page: 1,
+    transactions: [],
+    success: true
+  });
+});
+
+const port = 3000;
+server.listen(port, (err) => {
   if(!err) {
-    console.log('server is start at port 8080');
+    console.log(`server is start at port ${port}`);
   }
 });

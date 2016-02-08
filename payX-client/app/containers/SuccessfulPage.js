293419b25d9eff4e922f6e52bf16c2634263437e
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-08 13:07:54 +0700

    add actions for success page

--- payX-client/app/containers/SuccessfulPage.js
+++ payX-client/app/containers/SuccessfulPage.js
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux';
 import * as sendMoney from '../actions/sendMoney';
 import { routeActions } from 'react-router-redux'
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({...sendMoney, ...routeActions}, dispatch);
@@ -16,6 +17,20 @@ function mapStateToProps(state) {
 }
 
 class SuccessfulPage extends Component {
+
+  constructor() {
+    super();
+    this.onClickBackButton = this.onClickBackButton.bind(this);
+    this.onClickViewTransactions = this.onClickViewTransactions.bind(this);
+  }
+
+  onClickBackButton() {
+    this.props.goBack();
+  }
+
+  onClickViewTransactions() {
+    this.props.push('/viewTransactions')
+  }
 
   componentDidMount() {
     const {success} = this.props.response;
@@ -30,19 +45,22 @@ class SuccessfulPage extends Component {
       <input
         className='form-control bottom-left-button btn-warning'
         type="button"
-        onClick={() => {}}
+        onClick={this.onClickBackButton}
         value='Back'
       />,
       <input
         className='form-control bottom-right-button btn-primary'
         type="button"
-        onClick={() => {}}
+        onClick={this.onClickViewTransactions}
         value='View Transactions'
       />
     ];
 
     return (
       <div>
+        <Header
+          tittle='Send Money'
+        />
         <div>
           {`${this.props.response.from} sent to ${this.props.response.to}
             ${this.props.response.currency.symbol}${this.props.response.amount} `}

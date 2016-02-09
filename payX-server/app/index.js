6dc0011b23ad285c6e80b7f133a24fe018a037f6
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-09 11:26:27 +0700

    make the pagination in transactions history

--- payX-server/app/index.js
+++ payX-server/app/index.js
@@ -2,14 +2,38 @@ import 'babel-polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import bluebird from 'bluebird';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
 const server = express();
 
-function loadFile() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(() => {
-      resolve('foo')
-    }, 1000);
+const loadFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        resolve(err);
+      } else {
+        resolve(data);
+      }
+    });
   });
+}
+
+const paginate = (arrData, page, itemsPerPage = 10) => {
+  let transactions = _.filter(arrData, (item, idx) => {
+    return Math.floor(idx / itemsPerPage) === page;
+  });
+
+  let totalPages = Math.ceil(arrData.length / itemsPerPage);
+
+  return {
+    current_page: page,
+    total_pages: totalPages,
+    transactions: transactions,
+    per_page: itemsPerPage,
+    success: true
+  };
 }
 
 server.use(bodyParser.urlencoded({extended: true}));
@@ -23,13 +47,19 @@ server.post('/send_money', async (req, res) => {
 });
 
 server.get('/transactions_history', async (req, res) => {
-  res.status(200).json({
-    per_page: 10,
-    total_pages: 5,
-    current_page: 1,
-    transactions: [],
-    success: true
-  });
+
+  let transactions = await loadFile(path.join(__dirname, '../app/data', 'transactions.json'));
+
+  if (transactions instanceof Error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error when loading transactions',
+      codeMessage: transactions.toString()
+    });
+    return;
+  }
+
+  res.status(200).json(paginate(JSON.parse(transactions), 0, 10));
 });
 
 const port = 3000;

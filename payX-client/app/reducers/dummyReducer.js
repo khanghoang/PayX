400eb58ad65e4bbb512836b4f91b6a4ac827a06f
Khang Hoang <hoangtrieukhang@gmail.com>
2016-02-10 11:48:05 +0700

    remove dummy files

--- payX-client/app/reducers/dummyReducer.js
+++
@@ -1,7 +0,0 @@
-const intialState = {
-  foo: 'bar'
-}
-
-export default function update(state = intialState, action) {
-  return state;
-}
--- payX-client/app/reducers/index.js
+++ payX-client/app/reducers/index.js
@@ -1,3 +1,2 @@
-export dummyReducers from './dummyReducer';
 export sendMoneyReducer from './sendMoney';
 export fetchTransactionsReducer from './fetchTransactions';
